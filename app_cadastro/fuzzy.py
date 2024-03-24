6db2b414fa1a542f7acfb23f2e5c6561944b8f84
CeciliaBotelho <ceciliacbcbcb@gmail.com>
2024-03-24 00:17:42 -0300

    Organização dos arquivos

--- app_cadastro/fuzzy.py
+++ app_cadastro/fuzzy.py
@@ -14,31 +14,34 @@ def calcular_credito(idade, renda_mensal):
     # Faixas para crédito ajustadas para representar valores de 1500 a 60000
     credito = ctrl.Consequent(np.arange(1500, 60001, 1), 'credito')
 
+
     # Definindo os conjuntos fuzzy para idade
     idade_fuzzy['jovem'] = fuzz.trimf(idade_fuzzy.universe, [0, 30, 65])
     idade_fuzzy['medio'] = fuzz.trimf(idade_fuzzy.universe, [30, 65, 100])
     idade_fuzzy['idoso'] = fuzz.trimf(idade_fuzzy.universe, [65, 100, 100])
 
     # Definindo os conjuntos fuzzy para renda mensal
-    renda_mensal_fuzzy['pobre'] = fuzz.trimf(renda_mensal_fuzzy.universe, [0, 0, 5000])
-    renda_mensal_fuzzy['medio'] = fuzz.trimf(renda_mensal_fuzzy.universe, [5000, 10000, 15000])
-    renda_mensal_fuzzy['rico'] = fuzz.trimf(renda_mensal_fuzzy.universe, [15000, 20000, 60000])
+    renda_mensal_fuzzy['baixa'] = fuzz.trimf(renda_mensal_fuzzy.universe, [0, 0, 5000])
+    renda_mensal_fuzzy['media'] = fuzz.trimf(renda_mensal_fuzzy.universe, [2000, 7500, 20000])
+    renda_mensal_fuzzy['alta'] = fuzz.trimf(renda_mensal_fuzzy.universe, [15000, 30000, 60000])
 
     # Definindo os conjuntos fuzzy para crédito
     credito['baixo'] = fuzz.trimf(credito.universe, [1500, 1500, 21500])
     credito['medio'] = fuzz.trimf(credito.universe, [21500, 30000, 41500])
     credito['alto'] = fuzz.trimf(credito.universe, [41500, 60000, 60000])
 
-    # Definindo as regras fuzzy
-    regra1 = ctrl.Rule(idade_fuzzy['jovem'] & renda_mensal_fuzzy['rico'], credito['alto'])
-    regra2 = ctrl.Rule(idade_fuzzy['medio'] & renda_mensal_fuzzy['rico'], credito['alto'])
-    regra3 = ctrl.Rule(idade_fuzzy['idoso'] & renda_mensal_fuzzy['rico'], credito['medio'])
-    regra4 = ctrl.Rule(idade_fuzzy['medio'] & renda_mensal_fuzzy['medio'], credito['medio'])
-    regra5 = ctrl.Rule(idade_fuzzy['jovem'] & renda_mensal_fuzzy['medio'], credito['medio'])
-    regra6 = ctrl.Rule(idade_fuzzy['idoso'] & renda_mensal_fuzzy['medio'], credito['medio'])
-    regra7 = ctrl.Rule(idade_fuzzy['jovem'] & renda_mensal_fuzzy['pobre'], credito['medio'])
-    regra8 = ctrl.Rule(idade_fuzzy['medio'] & renda_mensal_fuzzy['pobre'], credito['baixo'])
-    regra9 = ctrl.Rule(idade_fuzzy['idoso'] & renda_mensal_fuzzy['pobre'], credito['baixo'])
+# Definindo as regras fuzzy
+    regra1 = ctrl.Rule(idade_fuzzy['jovem'] & renda_mensal_fuzzy['alta'], credito['alto'])
+    regra2 = ctrl.Rule(idade_fuzzy['medio'] & renda_mensal_fuzzy['alta'], credito['medio'])
+    regra3 = ctrl.Rule(idade_fuzzy['idoso'] & renda_mensal_fuzzy['alta'], credito['medio'])
+
+    regra4 = ctrl.Rule(idade_fuzzy['jovem'] & renda_mensal_fuzzy['media'], credito['medio'])
+    regra5 = ctrl.Rule(idade_fuzzy['medio'] & renda_mensal_fuzzy['media'], credito['baixo'])
+    regra6 = ctrl.Rule(idade_fuzzy['idoso'] & renda_mensal_fuzzy['media'], credito['baixo'])
+
+    regra7 = ctrl.Rule(idade_fuzzy['jovem'] & renda_mensal_fuzzy['baixa'], credito['baixo'])
+    regra8 = ctrl.Rule(idade_fuzzy['medio'] & renda_mensal_fuzzy['baixa'], credito['baixo'])
+    regra9 = ctrl.Rule(idade_fuzzy['idoso'] & renda_mensal_fuzzy['baixa'], credito['baixo'])
 
     # Criando o sistema de controle
     sistema_controle = ctrl.ControlSystem([regra1, regra2, regra3, regra4, regra5, regra6, regra7, regra8, regra9])
@@ -54,4 +57,4 @@ def calcular_credito(idade, renda_mensal):
     simulacao.compute()
 
     # Retornando o valor de crédito, adicionando 1500
-    return simulacao.output['credito'] + 1500
+    return simulacao.output['credito']
