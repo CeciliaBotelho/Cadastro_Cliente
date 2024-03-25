5e1c052afa7b5cc8c02c20eb341e5e8781eb48d6
CeciliaBotelho <ceciliacbcbcb@gmail.com>
2024-03-24 22:15:36 -0300

    Finalizado

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,8 @@ from app_cadastro import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Rota para a página de registro
+    path('', views.home, name='home'),  
     path('usuarios/', views.usuarios, name='listagem_usuarios'),
-    # Adicionando a nova URL para editar um usuário
     path('editar_usuario/<int:id_usuario>/', views.editar_usuario, name='editar_usuario'),
     path('deletar_usuario/<int:id_usuario>/', views.deletar_usuario, name='deletar_usuario'),
     path('cadastrar/', views.cadastrar_usuario, name='cadastrar_usuario'),  # Novo caminho para registrar usuário
